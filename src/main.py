522846422cf6b4eff76a87ad7d19ac99fad27cd7
Brendan Mosby <soltra45@protonmail.com>
2022-09-03 21:31:11 -0400

    Fixed bug where the program would check if the file exists after it has been downloaded causing it to always be true

--- src/main.py
+++ src/main.py
@@ -13,16 +13,16 @@ for asset in assets:
     file_ext = os.path.splitext(file_name)[1]
     url = asset[file_name]
     
+    if (os.path.exists(f"{DownloadAssets.USER_STEAM_DIR}/{file_name}")):
+        print("You already have the latest version!")
+        quit()
+
     DownloadAssets.download_assets(url, file_name)
 
     if (file_ext == ".sha512sum"):
         hash = file_name
     elif (file_ext == ".gz"):
         file = file_name
-
-if (os.path.exists(f"{DownloadAssets.USER_STEAM_DIR}/{file_name}")):
-    print("You already have the latest version!")
-    quit()
 
 DownloadAssets.compare_hash(file, hash)
 
